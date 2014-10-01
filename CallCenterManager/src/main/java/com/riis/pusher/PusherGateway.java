62c78225277b41da87f432c2a40ffc35ac314451
pmoon <pmoon@riis.com>
2014-10-01 17:08:50 -0400

    Added initial test for pusher

---
+++ CallCenterManager/src/main/java/com/riis/pusher/PusherGateway.java
@@ -0,0 +1,55 @@
+package com.riis.pusher;
+
+import java.util.Collections;
+
+import com.pusher.rest.Pusher;
+
+public class PusherGateway
+{
+    private String appId = "91457";
+    private String apiKey = "da81ce797d453db253d1";
+    private String apiSecret = "1b3278510bc037b2db85";
+
+    
+    
+    public String getAppId()
+    {
+        return appId;
+    }
+    
+    
+    public void setAppId(String appId)
+    {
+        this.appId = appId;
+    }
+    
+    
+    public String getApiKey()
+    {
+        return apiKey;
+    }
+    
+    
+    public void setApiKey(String apiKey)
+    {
+        this.apiKey = apiKey;
+    }
+    
+    
+    public String getApiSecret()
+    {
+        return apiSecret;
+    }
+
+    
+    public void setApiSecret(String apiSecret)
+    {
+        this.apiSecret = apiSecret;
+    }
+    
+    public void pushTestNotification()
+    {
+        Pusher pusher = new Pusher(appId, apiKey, apiSecret);
+        pusher.trigger("channel-one", "test_event", Collections.singletonMap("message", "hello world"));
+    }
+}
