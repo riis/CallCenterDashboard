d4084a1e1e5cc030b85babebc43f2ac20ced236a
cbeyer-riis <cbeyer@riis.com>
2014-10-01 15:34:28 -0400

    added headless gem

--- tests/cucumber/features/support/env.rb
+++ tests/cucumber/features/support/env.rb
@@ -2,3 +2,14 @@ require 'rspec'
 require 'page-object'
 
 World(PageObject::PageFactory)
+
+if ENV['HEADLESS'] == 'true'
+  require 'headless'
+
+  headless = Headless.new
+  headless.start
+
+  at_exit do
+    headless.destroy
+  end
+end
